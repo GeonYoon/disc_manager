61d3daaf014cb2f3a622a233295883bbd43cb094
kangjuw <juwon357@naver.com>
2018-12-16 04:27:47 +0900

    calcScore

--- backend/Scoring/calcScore.py
+++ backend/Scoring/calcScore.py
@@ -1,7 +1,6 @@
 ## scoring
 import sys
-
-input = sys.argv([1])
+import json
 
 ## load Model data
 with open('data.json', 'r') as file:
@@ -9,9 +8,16 @@ with open('data.json', 'r') as file:
 M_w = data["M"]
 F_w = data["F"]
 
-def Scoring(Input): ## ex input = ["M", 180, 70, 32, 43] ex sex / ht / wt / fat / mus
+def Scoring(input): ## ex input = ["M", 180, 70, 32, 43] ex sex / ht / wt / fat / mus
     if input[0] == "M":
         z_bmi = abs(((input[2]/((input[1])**2) - M_w[0])/M_w[1])*5)
         z_fat = abs(((input[3] - M_w[2])/M_w[3])*5)
         z_mus = abs(((input[4] - M_w[4])/M_w[5])*5)
-    return 100 - sum([z_bmi, z_fat, z_mus])
+        return 100 - sum([z_bmi, z_fat, z_mus])
+    if input[0] == "F":
+        z_bmi = abs(((input[2]/((input[1])**2) - F_w[0])/F_w[1])*5)
+        z_fat = abs(((input[3] - F_w[2])/F_w[3])*5)
+        z_mus = abs(((input[4] - F_w[4])/F_w[5])*5)
+        return 100 - sum([z_bmi, z_fat, z_mus])
+
+print(Scoring(input))
