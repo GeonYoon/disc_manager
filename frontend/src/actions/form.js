7421415227d6a49a6426069b2e44e2ffe96af9e3
geon yoon <geonyoon@geons-MacBook-Air.local>
2018-12-16 02:30:14 +0900

    add

--- frontend/src/actions/form.js
+++ frontend/src/actions/form.js
@@ -7,6 +7,6 @@ import {
 export const form = (age, height,weight,muscle_mass,body_fat,smoking,file,history) => async dispatch => {
   const compact = {age, height,weight,muscle_mass,body_fat,smoking,file}
   const res = await axios.post('http://127.0.0.1:8000/api/form/',compact)
-  dispatch({type: FORM_SUCCESS, payload: res.data})
+  dispatch({type: FORM_SUCCESS, payload: res.data, meta : compact})
   history.push('/home')
 }
--- frontend/src/reducers/form.js
+++ frontend/src/reducers/form.js
@@ -5,12 +5,25 @@ import { handleActions } from 'redux-actions';
 
 const formInitialState = {
   percentage : null,
+  age : null,
+  weight : null,
+  muscle_mass : null,
+  body_fat : null,
+  smoking : null,
+  file : null
 }
 
 export default handleActions({
   [FORM_SUCCESS] : (state, action) => {
+    console.log(action.meta);
     return {
-      percentage : action.payload.percentage
+      percentage : action.payload.percentage,
+      age : action.meta.age,
+      weight : action.meta.weight,
+      muscle_mass : action.meta.muscle_mass,
+      body_fat : action.meta.body_fat,
+      smoking : action.meta.smoking,
+      file : null
     }
   }
 }, formInitialState)
