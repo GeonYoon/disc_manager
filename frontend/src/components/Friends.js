bfd2e92612b87d3e0a00f21475e58e340733458f
phu54321 <phu54321@naver.com>
2018-12-16 01:26:36 +0900

    Basic friend list (sorted by bmi)

--- frontend/src/components/Friends.js
+++ frontend/src/components/Friends.js
@@ -1,13 +1,44 @@
-import React from 'react';
+import React from 'react'
+import friendsData from '../assets/friendsData'
+import { Table } from 'react-materialize'
+
+function bmi ({ height, weight }) {
+  return weight / height / height * 10000
+}
 
 const Friends = () => {
-    return (
-        <div style ={{ textAlign : 'center'}}>
-            <h1>
-            Friends!
-            </h1>
-        </div>
-    );
-};
+  const myData = {
+    name: '나',
+    height: 178,
+    weight: 85
+  }
+
+  const sortedData = friendsData.slice()
+    .concat([myData]).sort((a, b) => bmi(a) - bmi(b))
+  return (
+    <div style ={{ textAlign : 'center'}}>
+      <h1>Friends</h1>
+
+      <Table>
+        <thead>
+          <tr>
+            <th>이름</th>
+            <th>키</th>
+            <th>몸무게</th>
+            <th>BMI</th>
+          </tr>
+        </thead>
+        <tbody>
+          {sortedData.map(data => <tr key={data.name}>
+            <td>{data.name}</td>
+            <td>{data.height}</td>
+            <td>{data.weight}</td>
+            <td>{bmi(data).toFixed(1)}</td>
+          </tr>)}
+        </tbody>
+      </Table>
+    </div>
+  )
+}
 
 export default Friends;
