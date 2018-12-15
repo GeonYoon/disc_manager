aed4e41feb512db36a68bf317928691ed2a73348
geon yoon <geonyoon@geons-MacBook-Air.local>
2018-12-16 01:37:45 +0900

    fix wrong url

--- frontend/src/actions/form.js
+++ frontend/src/actions/form.js
@@ -8,5 +8,5 @@ export const form = (age, height,weight,muscle_mass,body_fat,smoking,file,histor
   const compact = {age, height,weight,muscle_mass,body_fat,smoking,file}
   const res = await axios.post('http://127.0.0.1:8000/api/form/',compact)
   dispatch({type: FORM_SUCCESS, payload: res.data})
-  history.push('/output')
+  history.push('/home')
 }
